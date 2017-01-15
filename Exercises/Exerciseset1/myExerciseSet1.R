2d99a692b44931333bde0ded0f0a4d2b17963d4b
sushantcj <sushant.chalipat@insead.edu>
2017-01-15 18:41:21 +0800

    ExerciseSet1 file commit

---
+++ Exercises/Exerciseset1/myExerciseSet1.R
@@ -0,0 +1,19 @@
+#Exercise Set 1
+
+#Check and change current working directory
+getwd()
+setwd("Exercises/ExerciseSet1")
+getwd()
+
+#list all files in the working directory
+list.files()
+
+#source data - ticker SPY and APPL 
+source("helpersSet1.R")
+source("dataSet1.R")
+
+#loads of finance functions including option pricing / sharpeRatio
+# https://cran.r-project.org/web/views/Finance.html
+
+
+
